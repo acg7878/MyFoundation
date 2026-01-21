79a5923553e0c8207ed53a0243a8e981ccb00e22
yangjunke <yangjunke@baidu.com>
2026-01-21 20:04:04 +0800

    Initial Commit

---
+++ MyFoundation/MyFoundation.h
@@ -0,0 +1,16 @@
+//
+//  MyFoundation.h
+//  MyFoundation
+//
+//  Created by Derek on 2026/1/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyFoundation.
+FOUNDATION_EXPORT double MyFoundationVersionNumber;
+
+//! Project version string for MyFoundation.
+FOUNDATION_EXPORT const unsigned char MyFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFoundation/PublicHeader.h>
